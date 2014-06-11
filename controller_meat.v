821c55179aede57bccdc02ffb7b6b0290522a3a5
FredericoButzke <butzkefrederico@gmail.com>
2014-06-10 17:39:01 -0700

    Controler MEAT v0.02

--- controller_meat.v
+++ controller_meat.v
@@ -15,25 +15,12 @@ module controller(
 
 wire y0 ;
 
-//Y0 = RACK~*ERR0 + RACK~*ERR1 + Y0*RACK~*REREQ
-assign #1 y0 = ~Rack & Err0 | ~Rack & Err1 | y0 & ~Rack & REreq;
-
-//LACK = LEACK
+assign #1 y0 = LEack & ~REreq | y0 & LEack & ~Err1 & ~Err0;
 assign #1 Lack = LEack;
-
-//RREQ = LEACK
 assign #1 Rreq = LEack;
-
-//LEREQ = LREQ
 assign #1 LEreq = Lreq;
-
-//REACK = RACK + Y0*REREQ
-assign #1 REack = Rack | y0 & REreq;
-
-//CLK = LEACK*REREQ~
+assign #1 REack = ~y0 & REreq;
 assign #1 clk = LEack & ~REreq;
-
-//SAMPLE = Y0~*RACK~*REREQ
-assign #1 sample = ~y0 * ~Rack * REreq;
+assign #1 sample = y0 & REreq;
 
 endmodule
