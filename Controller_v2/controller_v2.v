968bb219c16b07a42a2d053dd5c89cc67c5d3a3f
FredericoButzke <butzkefrederico@gmail.com>
2014-06-19 11:59:44 -0700

    Controller V2.2.

--- Controller_v2/controller_v2.v
+++ Controller_v2/controller_v2.v
@@ -19,20 +19,17 @@ module controller(
 	wire a, b;						
 	
 	//internal signals
-	wire goLR, goME, goML, goEM, goLM ;
+	wire goML, goLM ;
 
 	assign #1 Lack = goML ;
 	assign #1 Rreq = goLM & ~Rack | goLM & Rreq | ~Rack & Rreq & rst ;
 	assign #1 LEreq = Lreq ;
-	assign #1 REack = goME ;
-	assign #1 clk = ~goLM & Rack & ~Err1 & ~Err0 & goME | goLM & ~Rack & ~Err1 & ~Err0 & ~goME ;
-	assign #1 sample = ~goEM & ~a & ~b & goME | goEM & ~a & ~b & ~goME ;
+	assign #1 REack = goML & REack & rst | b & Rreq | a & Rreq | ~a & ~b & REack & rst ;
+	assign #1 clk = ~goLM & Rack & ~Err1 & ~Err0 & REack | goLM & ~Rack & ~Err1 & ~Err0 & ~REack ;
+	assign #1 sample = ~REreq & ~a & ~b & REack | REreq & ~a & ~b & ~REack ;
 
-	assign #1 goEM = REreq ;
-	assign #1 goLR = LEack ;
 	assign #1 goLM = LEack ;
-	assign #1 goML = ~sample & goML & rst | Err1 & Rreq | Err0 & Rreq | ~Err1 & ~Err0 & goME ;	
-	assign #1 goME = goML & goME & rst | b & Rreq | a & Rreq | ~a & ~b & goME & rst ;
+	assign #1 goML = ~sample & goML & rst | Err1 & Rreq | Err0 & Rreq | ~Err1 & ~Err0 & REack ;	
 
 	assign #1 a = Err0 ;
 	assign #15 b = Err1;
